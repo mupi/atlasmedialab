7b9d90221406fd26df898de6be03caaf8d025fde
Ana Rute <anarutecvm@gmail.com>
2014-09-09 20:13:37 -0300

    Home: change background colors of elements

--- front-page.php
+++ front-page.php
@@ -14,13 +14,25 @@
 		<?php $latest = new WP_Query('posts_per_page=2&post_type=post&category_name=noticias'); if ($latest->have_posts()) : ?>
 		<?php while ($latest->have_posts()) : $latest->the_post(); ?>
 		<div class="col-sm-4">
-			<article class="new">
-				<h4 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-				<span class="date"><?php the_time('d/m/Y'); ?></span>
-				<div class="description">
-					<?php the_excerpt(); ?>
-				</div>
-			</article>
+			<a href="<?php the_permalink(); ?>" class="new-anchor">
+				<article class="new">
+					<?php
+					if ( '' != get_the_post_thumbnail() ) {
+					?>
+						<header class="news-header">
+							<?php echo get_the_post_thumbnail(); ?>
+						</header>
+					<?php
+					} ?>
+					<div class="news-body">
+						<h4 class="title"><?php the_title(); ?></h4>
+	<!-- 					<span class="date"><?php the_time('d/m/Y'); ?></span> -->
+						<div class="description">
+							<?php the_excerpt(); ?>
+						</div>
+					</div>
+				</article>
+			</a>
 		</div>
 		<?php endwhile; endif; wp_reset_postdata(); ?>
 	</section>
